116ff891f7e40b2d7d0bc2b723ccef7def180838
Jacob Bijani <jacob.bijani@gmail.com>
2013-06-28 15:36:40 -0700

    stream and emitters

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 
+var emitter = require('./lib/emitter');
+var stream  = require('./lib/stream');
+
 var app = express();
+
+stream.start();
 
 app.configure(function(){
     app.use(express.static(__dirname + '/public'));
@@ -11,8 +16,15 @@ app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.get('/*', function(req, res){
-    res.end();
+app.get('/stream/:name', function(req, res){
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    res.write('\n');
+
+    stream.register(req.params.name, res);
 });
 
 var port = process.env.PORT || 5000;
